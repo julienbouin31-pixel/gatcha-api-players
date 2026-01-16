1e08934d17338746d1ff44ac1f5f99758f0f78ef
Julien <julienbouin31@gmail.com>
2026-01-16 11:13:38 +0100

    add basic maven config

---
+++ src/main/java/fr/imt/gatcha/gatcha_api_players/GatchaApiPlayersApplication.java
@@ -0,0 +1,13 @@
+package fr.imt.gatcha.gatcha_api_players;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatchaApiPlayersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatchaApiPlayersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/imt/gatcha/gatcha_api_players/GatchaApiPlayersApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.imt.gatcha.gatcha_api_players;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatchaApiPlayersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
